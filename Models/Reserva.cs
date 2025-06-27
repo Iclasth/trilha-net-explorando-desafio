586fd86e1b29ab7eed9174ffbac5b857717398eb
iclasTH <iclasinprogramacao@gmail.com>
2025-06-27 19:42:36 -0300

    Modificando o Sistema de Hospedagem

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -1,3 +1,4 @@
+using System;
 namespace DesafioProjetoHospedagem.Models
 {
     public class Reserva
@@ -15,14 +16,17 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
+           
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (Suite.Capacidade >= hospedes.Count)
             {
+                
                 Hospedes = hospedes;
             }
             else
             {
+                throw new ArgumentException("Capacidade excedida: a quantidade de hóspedes não pode exceder a capacidade da suíte.");
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
             }
@@ -35,23 +39,21 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            
+            int quantidade = Hospedes.Count;
+            return quantidade;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                decimal desconto = 0.1M; // 10% de desconto
+                valor = (DiasReservados*Suite.ValorDiaria) * desconto;
             }
 
             return valor;
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using DesafioProjetoHospedagem.Models;
 
 Console.OutputEncoding = Encoding.UTF8;
@@ -6,20 +7,39 @@ Console.OutputEncoding = Encoding.UTF8;
 // Cria os modelos de hóspedes e cadastra na lista de hóspedes
 List<Pessoa> hospedes = new List<Pessoa>();
 
+//Adicionando os Hóspedes
 Pessoa p1 = new Pessoa(nome: "Hóspede 1");
 Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+Pessoa p3 = new Pessoa(nome: "Hóspede 3");
 
-hospedes.Add(p1);
-hospedes.Add(p2);
+bool menu = true;
 
-// Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+while (menu)
+{
 
-// Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
-reserva.CadastrarSuite(suite);
-reserva.CadastrarHospedes(hospedes);
+    Console.Clear();
+    Console.WriteLine("Seja bem-vindos ao Hotel Dev!");
 
-// Exibe a quantidade de hóspedes e o valor da diária
-Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+    hospedes.Add(p1);
+    hospedes.Add(p2);
+    hospedes.Add(p3);
+
+    // Cria a suíte
+    Suite suite = new Suite(tipoSuite: "Master", capacidade: 3, valorDiaria: 45);
+
+    // Cria uma nova reserva, passando a suíte e os hóspedes
+    Reserva reserva = new Reserva(diasReservados: 12);
+    reserva.CadastrarSuite(suite);
+    reserva.CadastrarHospedes(hospedes);
+
+    // Exibe a quantidade de hóspedes e o valor da diária
+    Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+    Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+
+    Console.WriteLine("\nPressione qualquer tecla para continuar ou 'S' para sair.");
+    var opcao = Console.ReadKey().Key;
+    if (opcao == ConsoleKey.S)
+    {
+        menu = false;
+    }
+}
